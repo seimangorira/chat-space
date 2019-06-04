57cb775adc79530a7035efdcb8484fbf08fda1b4
seimangorira <seimangorira@gmail.com>
2019-06-04 21:07:32 +0900

    CreateModellTest

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  congig.include FactoryBot::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
