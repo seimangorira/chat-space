f424c5a3f99fc3034dccad2d380dd10c27a6736d
seimangorira <seimangorira@gmail.com>
2019-06-19 20:46:49 +0900

    apdateAPI

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,9 +1,10 @@
 $(function(){
 　var cancelFlg = 0;
 
-  function buildMessage(message){
-    console.log(message);
-    if(message.content  && message.image.url ) {
+  function buildMessage(message) {
+    var content = message.content? message.content : ""
+    var image = message.image.url? message.image.url : ""
+
     var html = `<div class="message">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
@@ -15,49 +16,12 @@ $(function(){
                   </div>
                   <div class="lower-message">
                     <p class="lower-message__content">
-                      ${message.content}
+                      ${content}
                     </p>
                   </div>
-                  <img class="lower-message__image" src=${message.image.url}>
+                  <img class="lower-message__image" src=${image}>
                 </div>`
       return html;
-    } else if(message.content){
-      var html = `<div class="message">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                        ${message.content}
-                      </p>
-                    </div>
-                  </div>`
-        return html;
-    } else if(message.image.url){
-      var html = `<div class="message">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                        ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <img class="lower-message__image" src=${message.image.url}>
-                    </div>
-                  </div>`
-        return html;
-    } else {
-      $(function(){
-        alert("入力内容がありません");
-      })
-    }
   }
 
   $("#new_message").on("submit", function(e){
@@ -79,7 +43,7 @@ $(function(){
       $("#new_message")[0].reset();
     })
     .fail(function(message){
-      alert("エラー");
+      alert("メッセージを入力してください");
     })
     .always(function(message){
       $(".form__submit").prop("disabled", false);
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,16 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    @message.save
+    if @message.save
+      respond_to do |format|
+      format.html {redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'}
+      format.json
+      end
+    else
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
   end
 
   private
