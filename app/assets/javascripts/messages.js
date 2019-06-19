7ad19b61c39d45330e8790165ea03d6e7b7f0921
seimangorira <seimangorira@gmail.com>
2019-06-19 19:34:15 +0900

    update API

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -2,6 +2,8 @@ $(function(){
 　var cancelFlg = 0;
 
   function buildMessage(message){
+    console.log(message);
+    if(message.content  && message.image.url ) {
     var html = `<div class="message">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
@@ -15,10 +17,47 @@ $(function(){
                     <p class="lower-message__content">
                       ${message.content}
                     </p>
-                    <img class="lower-message__image" src="${message.image}">
                   </div>
+                  <img class="lower-message__image" src=${message.image.url}>
                 </div>`
-    return html;
+      return html;
+    } else if(message.content){
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+        return html;
+    } else if(message.image.url){
+      var html = `<div class="message">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <img class="lower-message__image" src=${message.image.url}>
+                    </div>
+                  </div>`
+        return html;
+    } else {
+      $(function(){
+        alert("入力内容がありません");
+      })
+    }
   }
 
   $("#new_message").on("submit", function(e){
@@ -36,8 +75,8 @@ $(function(){
     .done(function(message){
       var html = buildMessage(message);
       $(".messages").append(html);
-      $("#message_content").val("");
       $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight}, "fast");
+      $("#new_message")[0].reset();
     })
     .fail(function(message){
       alert("エラー");
@@ -45,5 +84,7 @@ $(function(){
     .always(function(message){
       $(".form__submit").prop("disabled", false);
     })
+
+
   })
 });
